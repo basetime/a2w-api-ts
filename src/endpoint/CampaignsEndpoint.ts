a24d27ef32c45482f3d1eb9cf68e85dff20e318b
Sean Hickey <sean@headzoo.io>
2025-06-29 15:42:42 -0400

    Updates campaigns.createEnrollment

--- src/endpoint/CampaignsEndpoint.ts
+++ src/endpoint/CampaignsEndpoint.ts
@@ -22,6 +22,11 @@ const enrollmentEndpoint = '/e';
  */
 export default class CampaignsEndpoint extends Endpoint {
   /**
+   * A function to encode the data into a jwt. Used by the enroolment endpoint.
+   */
+  public jwtEncode?: (data: Record<string, any>) => Promise<string>;
+
+  /**
    * Returns all of the campaigns for authenticated organization.
    *
    * @returns The campaigns.
@@ -115,6 +120,9 @@ export default class CampaignsEndpoint extends Endpoint {
   /**
    * Creates an enrollment for a campaign, and returns the bundle ID and any errors.
    *
+   * This method needs to encode the data into a jwt. The jwt is used to authenticate
+   * with the site. This method requires the jwtEncode function to be set.
+   *
    * @param campaignId The ID of the campaign.
    * @param metaValues The meta values to set.
    * @param formValues The form values to set.
@@ -124,12 +132,21 @@ export default class CampaignsEndpoint extends Endpoint {
     metaValues: MetaValues = {},
     formValues: Record<string, any> = {},
   ): Promise<EnrollmentResponse> => {
+    if (!this.jwtEncode) {
+      throw new Error(
+        'CampaignsEndpoint.createEnrollment() requires the jwtEncode function to be set.',
+      );
+    }
+
+    const body = {
+      d: await this.jwtEncode({
+        metaValues,
+        formValues,
+      }),
+    };
     const url = `${enrollmentEndpoint}/campaign/${campaignId}`;
 
-    return await this.doPost<EnrollmentResponse>(url, {
-      metaValues,
-      formValues,
-    });
+    return await this.doPost<EnrollmentResponse>(url, body);
   };
 
   /**
